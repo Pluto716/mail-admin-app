f0d7768fe8f9acc0bccffeb2fb9ba0d15d1b919a
Pluto716 <2261598840@qq.com>
2021-10-04 11:34:08 +0800

    整体的布局

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 用于切换闭合菜单，false为不闭合，true为闭合
+    collapsed: false,
   },
   mutations: {
+    ChangeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    ChangeCollapsed({ commit }) {
+      commit('ChangeCollapsed');
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
